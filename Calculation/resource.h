778d8b316ceb49ab9a0e76b30bad2e4c73eac5a5
us-Song <dmltjr05@naver.com>
2021-07-23 14:06:43 +0900

    디자인 초안

--- Calculation/resource.h
+++ Calculation/resource.h
@@ -7,14 +7,32 @@
 #define IDS_ABOUTBOX                    101
 #define IDD_CALCULATION_DIALOG          102
 #define IDR_MAINFRAME                   128
+#define IDC_Num1                        1000
+#define IDC_Num2                        1001
+#define IDC_Num3                        1002
+#define IDC_plus                        1003
+#define IDC_Num4                        1004
+#define IDC_Num5                        1005
+#define IDC_Num6                        1006
+#define IDC_minus                       1007
+#define IDC_Num7                        1008
+#define IDC_Num8                        1009
+#define IDC_Num9                        1010
+#define IDC_multiply                    1011
+#define IDC_BUTTON13                    1012
+#define IDC_Num0                        1012
+#define IDC_divide                      1013
+#define IDC_result                      1014
+#define IDC_EDIT1                       1015
+#define IDC_erase                       1016
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        129
+#define _APS_NEXT_RESOURCE_VALUE        130
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_CONTROL_VALUE         1017
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
